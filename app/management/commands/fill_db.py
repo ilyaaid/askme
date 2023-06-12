7bf219eebd29f941ab002e5bbee5680c04cb2e73
Ilya Aleshin <idaleshin@mail.ru>
2023-06-12 22:26:53 +0300

    template for fill_db done

--- app/management/commands/fill_db.py
+++ app/management/commands/fill_db.py
@@ -1,11 +1,23 @@
 from django.core.management.base import BaseCommand
 
+
+
+def fill_with_ratio(ratio):
+    pass
+
+
+
 class Command(BaseCommand):
     help = "Filling the database"
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument("ratio", nargs="+", type=int)
+    def add_arguments(self, parser):
+        parser.add_argument("ratio", type=int, help="Введите коэффициент заполнения")
 
     def handle(self, *args, **options):
-        print("hello!")
+        ratio = options.get('ratio')
+        self.stdout.write("Коэффициент: " + str(ratio))
+        fill_with_ratio(ratio)
+
+
+
 
